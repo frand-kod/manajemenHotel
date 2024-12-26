a1267b95e2d3c546029926c5d6d6f4ff28b86be1
frand-kod <85376495+frand-kod@users.noreply.github.com>
2024-12-26 14:39:20 +0700

    Add files via upload

---
+++ Sistem_Manajemen_Hotel/Controler/ClientController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sistem_Manajemen_Hotel.Model.Entity;
+using Sistem_Manajemen_Hotel.Model.Context;
+using Sistem_Manajemen_Hotel.Model.Repository;
+
+namespace Sistem_Manajemen_Hotel.Controller
+{
+    public class ClientController
+    {
+        private ClientRepository _clientRepository;
+
+        public int Create (ClientEntity client)
+        {
+            int result = 0;
+            if (string.IsNullOrEmpty(client.Id))
+            {
+                MessageBox.Show("Id client harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (string.IsNullOrEmpty(client.Firstname))
+            {
+                MessageBox.Show("Nama client harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (string.IsNullOrEmpty(client.Lastname))
+            {
+                MessageBox.Show("Lastname client harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            if (client.Phone == null)
+            {
+                MessageBox.Show("Phone No harus diisi !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _clientRepository = new ClientRepository(context);
+                result = _clientRepository.CreateClient(client);
+            }
+            return result;
+        }
+        public List<ClientEntity> ReadAll()
+        {   
+            // membuat objek collection
+            List<ClientEntity> list = new List<ClientEntity>();
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _clientRepository = new ClientRepository(context);
+                list = _clientRepository.GetAllClients();
+            }
+            return list;
+        }
+    }
+}
---
+++ Sistem_Manajemen_Hotel/Model/Context/DbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Data.SQLite;
+using System.Diagnostics;
+
+namespace Sistem_Manajemen_Hotel.Model.Context
+{
+    public class DbContext : IDisposable
+    {
+        // deklarasi private variabel / field
+        private SQLiteConnection _conn;
+        private string connectionString;
+
+
+        // deklarasi property Conn (connection), untuk menyimpan objek koneksi
+        public SQLiteConnection Conn
+        {
+            get { return _conn ?? (_conn = GetOpenConnection()); }
+        }
+
+        // Method untuk melakukan koneksi ke database
+        private SQLiteConnection GetOpenConnection()
+        {
+            SQLiteConnection conn = null; // deklarasi objek connection
+
+            try // penggunaan blok try-catch untuk penanganan error
+            {
+                string dbPath = "C:\\Users\\user\\Documents\\kuliah\\Semester 3\\Pemrograman Lanjut\\Project dewe\\TesterMVC-SqlIite\\database.db";
+                connectionString = $"Data Source={dbPath};Version=3;";
+                var connection = new SQLiteConnection(connectionString);
+                connection.Open();
+
+                Debug.Print("Koneksi ke database berhasil dilakukan");
+            }
+            // jika terjadi error di blok try, akan ditangani langsung oleh blok catch
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("Open Connection Error: {0}", ex.Message);
+            }
+
+            return conn;
+        }
+
+        // Method ini digunakan untuk menghapus objek koneksi dari memory ketika sudah tidak digunakan
+        public void Dispose()
+        {
+            if (_conn != null)
+            {
+                try
+                {
+                    if (_conn.State != ConnectionState.Closed) _conn.Close();
+                }
+                finally
+                {
+                    _conn.Dispose();
+                }
+            }
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
